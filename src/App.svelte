a50d64387ea7139a10d3b9d3df952fcb619ed574
Daria Pashkova <dar.pashcovaa@gmail.com>
2024-07-24 16:22:17 +0900

    feat: update handleDateSelected function

--- src/App.svelte
+++ src/App.svelte
@@ -3,8 +3,20 @@
 
 	let selectedDates = ['2024-08-15', '2024-07-20', '2024-07-25'];
 
+	let chosenDates = new Set();
+
+	function formatDateToYYYYMMDD(dateString) {
+		const date = new Date(dateString);
+		const year = date.getFullYear();
+		const month = String(date.getMonth() + 1).padStart(2, '0');
+		const day = String(date.getDate()).padStart(2, '0');
+		return `${year}-${month}-${day}`;
+	}
+
 	function handleDateSelected(event) {
-		console.log('Выбрана дата:', event.detail);
+		document.body.classList.add('hidden');
+
+		chosenDates.add(formatDateToYYYYMMDD(event.detail));
 	}
 </script>
 
